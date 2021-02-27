2ad6668fd48a5727b88ea8355b30c3db54b24820
Raoul W <raoulwadhwa@gmail.com>
2021-02-27 11:54:25 -0500

    added xml1 testing

--- tests/testthat/test-convert.R
+++ tests/testthat/test-convert.R
@@ -1,12 +1,22 @@
 test_that("test TXT conversion", {
   # convert sample file w/ internal function
   tester <- txt_to_df_r(input_file = "TXT-sample-2")
-
+  
   ## confirm output accuracy
   # basic dimensions
   expect_equal(nrow(tester), 2)
   expect_equal(ncol(tester), 8)
-
+  
   # exactly 1 missing value in sample data
   expect_equal(sum(is.na(tester)), 1)
+})
+
+test_that("test XML1 conversion", {
+  # convert sample file w/ internal function
+  tester <- xml1_to_df_r(input_file = "XML-sample-14")
+  
+  ## confirm output accuracy
+  # basic dimensions
+  expect_equal(nrow(tester), 14)
+  expect_equal(ncol(tester), 8)
 })
